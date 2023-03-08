f251a359681de2b7ea313ed55065fc93caca5891
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-08 18:04:34 +0100

    add global variables to javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+const email = document.querySelector('#email')
+const form = document.querySelector('#form')
+const password = document.querySelector('#password')
+
