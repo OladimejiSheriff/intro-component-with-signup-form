6891e869b1638bc1e66228d3f971615153af9a07
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-08 18:19:44 +0100

    fix bugs

--- script.js
+++ script.js
@@ -12,5 +12,22 @@ const firstNameValue = firstName.value.trim()
 const lastNameValue = lastName.value.trim()
 const emailValue = email.value.trim()
 const passwordValue = password.value.trim()
+if(firstNameValue === ''){
+  setError(firstName, 'First name cannont be empty')
+}
+else{
+  setSuccess(firstName)
+}
 
+}
+
+const setError = (input, msg) =>{
+const formControl = input.parentElement
+const  errorMsg = formControl.querySelector('small')
+errorMsg.innerText = msg
+formControl.className = 'form-control error'
+}
+const setSuccess = (input) => {
+  const formControl = input.parentElement
+  formControl.className = 'form-control success'
 }
