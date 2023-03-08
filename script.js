7e51e6ed99d0aca99762d30ed31aec4c48fcc403
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-08 18:08:31 +0100

    add local variables

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const lastName = document.querySelector('#last-name')
 const email = document.querySelector('#email')
 const form = document.querySelector('#form')
 const password = document.querySelector('#password')
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  validateForm()
+})
+const validateForm = ()=>{
+const firstNameValue = firstName.value.trim()
+const lastNameValue = lastName.value.trim()
+const emailValue = email.value.trim()
+const passwordValue = password.value.trim()
 
+}
